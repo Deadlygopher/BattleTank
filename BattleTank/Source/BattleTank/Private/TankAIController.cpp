74066fddfb763b7d9162acd82641b134a5a74100
Deadlygopher <halfutdinov@yandex.ru>
2022-03-12 22:44:22 +0500

    Lesson 119
    Добавили обнаружение танка игрока танками АИ

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn)
+	{
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerPawn);
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//auto ControlledTank = GetControlledTank();
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller have not detect player"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller have Detect: %s"), *(PlayerTank->GetName()))
+	}
+}
